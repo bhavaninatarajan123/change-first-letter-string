d034ac3f215e6367312f60cd93d1421bf8b70095
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-27 20:13:03 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,7 @@
+var a=prompt ("enter the value");
+document.write("the given value is"+" "+a+"<br>");
+var b=a.replace(a[0],a[0].toUpperCase());
+document.write("The capitalized word is ")
+document.write(b);
+
+
